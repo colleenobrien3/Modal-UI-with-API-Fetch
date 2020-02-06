e3d8c027ddbf17606f09e1473eae15dc892931c1
Colleen_O'Brien <colleen3117@gmail.com>
2020-02-06 10:05:57 -0500

    have popup windows, but position wrong

--- script.js
+++ script.js
@@ -10,20 +10,18 @@ fetch(baseUrl)
   })
   .then(res => {
   	newElement = document.createElement('span')
-  	newElement.innerText = res.name
   	newElement.classList.add('div')
-  	let newImage = document.createElement('img')
-  	newImage.setAttribute('src', res.image)
+    newElement.style.backgroundImage=`url(${res.image})`
   	let level1 = document.querySelector('.level1')
   	level1.appendChild(newElement)
   	console.log(newElement)
   	let newModal = document.createElement('span')
-	newModal.classList.add('modal')
+	 newModal.classList.add('modal')
 	newModal.classList.add('hidden')
 	newElement.appendChild(newModal)
-	newElement.appendChild(newImage)
 	newElement.addEventListener('click', popUp)
 	function popUp(event){
+    console.log(event.target)
 	newModal.classList.toggle('hidden')
 }
 	newModal.addEventListener('click', hide)
